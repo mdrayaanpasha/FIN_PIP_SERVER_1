6820461175d5c94257c451724e1c81f9c1f4495e
mohammed rayaan pasha <mdrayaanpasha@gmail.com>
2026-03-12 11:27:43 +0530

    first working prototype

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
+// s_1/index.js — producer
+import { Kafka } from 'kafkajs';
+import express from 'express';
+import fs from 'fs';
 import cron from "node-cron";
-import express from "express";
+import dotenv from 'dotenv';
+import { fetchAll } from './services/datapull.js';
+import { createClient } from 'redis';
+dotenv.config();
+
+const client = createClient({ url: process.env.REDIS_URL });
+client.on('error', err => console.error('Redis error:', err));
+await client.connect();
+
+
+// Write certs from env to /tmp at startup
+fs.writeFileSync('/tmp/service.cert', Buffer.from(process.env.SERVICE_CERT, 'base64'));
+fs.writeFileSync('/tmp/service.key', Buffer.from(process.env.SERVICE_KEY, 'base64'));
+
+
+const kafka = new Kafka({
+  brokers: [process.env.KAFKA_URL],
+  ssl: {
+    ca: [fs.readFileSync('./ca.pem', 'utf-8')],
+    cert: fs.readFileSync('/tmp/service.cert', 'utf-8'),
+    key: fs.readFileSync('/tmp/service.key', 'utf-8'),
+  }
+});
+
+const producer = kafka.producer();
+await producer.connect();
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post('/order', async (req, res) => {
+  const ohlcv = await fetchAll();
+  console.log(ohlcv);
+  await producer.send({
+    topic: 'orders',
+    messages: [{ key: 'user-42', value: JSON.stringify(req.body) }]
+  });
+  res.json({ status: 'sent' });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(3000, () => console.log('Producer on 3000'));
 
 
-cron.schedule("* * * * *", () => {
-  console.log("Running a task every minute");
+cron.schedule("*/1 * * * *", async () => {
+  const data = await fetchAll();
+  await client.setEx('latest_ohlcv', 600, JSON.stringify(data));
+
+  const topics = ["anomaly", "indicator", "persistence"];
+  for (const topic of topics) {
+    await producer.send({
+      topic,
+      messages: [{ key: 'ohlcv', value: "redis" }]
+    });
+  }
+  console.log("Pushed OHLCV to all topics");
 });
